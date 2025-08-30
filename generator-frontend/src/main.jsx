84b4744f45af75221b1e74132e0e2030ad7645f6
aleezayshahzad12 <aleezay.shahzad12@gmail.com>
2025-08-30 06:14:34 +0000

    Add frontend source code and configuration files

---
+++ generator-frontend/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CalorieFront from './CalorieFront'; 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <CalorieFront />
+  </React.StrictMode>
+);
